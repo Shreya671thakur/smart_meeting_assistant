a357620f584bbd65ff02735f537a0675329ad3d9
Shreya671thakur <163099007+Shreya671thakur@users.noreply.github.com>
2025-11-22 23:20:49 +0530

    audio_preprocessing.py

--- audio_preprocessing.py
+++ audio_preprocessing.py
@@ -5,12 +5,14 @@ import subprocess
 model = whisper.load_model("base")
 
 def transcribe_audio_file(file_path):
-    # Ensure ffmpeg can read the file
     output_wav = "temp.wav"
+
     subprocess.run([
         "ffmpeg", "-i", file_path, "-ar", "16000", "-ac", "1", output_wav, "-y"
     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    result = model.transcribe(output_wav)
+    result = model.transcribe(output_wav, verbose=False)
+
     os.remove(output_wav)
-    return result["text"]
+
+    return result["text"], result["segments"]
